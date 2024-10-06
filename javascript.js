1912fec2e6e7a4e0cbd1e2c47cf1df6920d8e0d5
Muhammad Sameer <126315978+Malik-Sameer@users.noreply.github.com>
2024-10-06 21:31:26 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,5 @@
+function onClickMenu(){
+    document.getElementById("nav-manu").classList.toggle("active")
+    document.getElementById("hamburger").classList.toggle("active")
+    document.getElementById("total").classList.toggle("active")
+}
